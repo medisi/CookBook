ffd02207cca9053eed95cef0da7b3df1cb080a24
medisi <111913427+medisi@users.noreply.github.com>
2025-11-15 22:12:23 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,5 @@ app.use('/api/recipes', require('./routes/recipes'));
 app.use('/api/categories', require('./routes/categories'));
 app.use('/api/profile', require('./routes/profile'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
