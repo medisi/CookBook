f6b72e36cb54ee54cd010ff91f41b894cd60a2ad
medisi <111913427+medisi@users.noreply.github.com>
2025-11-17 12:13:52 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'https://cookbook-j58r.onrender.com/',
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+    baseURL: process.env.REACT_APP_API_URL || 'https://cookbook-j58r.onrender.com/api',
 });
 
 // добавление интерцептора для токена
